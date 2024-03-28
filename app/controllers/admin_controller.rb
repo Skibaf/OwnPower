515bcfc9cfcb31b45f0c27654fe034185e91498e
skibaf <skiba_florencia@yahoo.com.ar>
2024-03-28 19:04:15 -0300

    mini fix carrito

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,8 @@
 class AdminController < ApplicationController
-  layout 'adminpanel'
+  
   before_action :authenticate_user!
   
   def index
   end
 
-  def users
-     @users=User.all
-  end
 end
--- app/controllers/coach_controller.rb
+++ app/controllers/coach_controller.rb
@@ -1,13 +1,11 @@
 class CoachController < ApplicationController
-  layout 'adminpanel'
+  
   before_action :authenticate_user!
   
   def index
    
-   @lessons = Lesson.all
    @mylessons = Lesson.all.where(coach: current_user)
     
-  
   end
 
 
